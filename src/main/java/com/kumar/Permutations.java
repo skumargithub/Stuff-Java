010abea118027355793f670e6aa1a79f915520ce
Kumar <skumarjunk@gmail.com>
2020-12-10 13:23:50 -0700

    Generate Permutations

---
+++ src/main/java/com/kumar/Permutations.java
@@ -0,0 +1,60 @@
+package com.kumar;
+
+import java.util.*;
+
+public class Permutations {
+    public static List<List<Integer>> permutations(List<Integer> input, int r) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Cannot pick r out of input less than r and nothing to pick if r == 0
+        if(input.size() < r || r == 0)  return result;
+
+        // Pick 1 element from the input
+        // Simply each element
+        if(r == 1) {
+            for(Integer inp : input) {
+                List<Integer> inpL = new ArrayList<>();
+                inpL.add(inp);
+
+                result.add(inpL);
+            }
+
+            return result;
+        }
+
+        List<Integer> inputClone = new ArrayList<>(input);
+        Integer first = inputClone.remove(0);
+
+        // First IS NOT contained in the answer
+        // Pick r from remaining (n - 1) elements
+        result.addAll(permutations(inputClone, r));
+
+        // First IS contained in the answer
+        // Pick (r - 1) from remaining (n - 1) elements
+        List<List<Integer>> r1ps = permutations(inputClone, r - 1);
+
+        // And add first to every possible place
+        for(List<Integer> r1p : r1ps) {
+            for(int i = 0; i <= r1p.size(); ++i) {
+                List<Integer> rc = new ArrayList<>(r1p);
+                rc.add(i, first);
+
+                result.add(rc);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.err.println("Hello Permutations!");
+
+        List<Integer> input = new ArrayList<>();
+        input.add(0);
+        input.add(1);
+        input.add(2);
+
+        List<List<Integer>> result = permutations(input, 3);
+        System.err.println(result);
+    }
+}
---
+++ src/test/java/com/kumar/PermutationsTest.java
@@ -0,0 +1,129 @@
+package com.kumar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class PermutationsTest {
+    public static int factorial(int n) {
+        int result = 1;
+        for(int i = 1; i <= n; ++i) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public int getRandomNumber(int min, int max) {
+//        System.err.println("min: " + min + ", max: " + max);
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
+
+    @Test
+    public void p0()
+    {
+        List<Integer> input = new ArrayList<>();
+        input.add(0);
+        input.add(1);
+        input.add(2);
+
+        List<List<Integer>> result = Permutations.permutations(input, 0);
+        assertTrue( result.size() == 0);
+    }
+
+    @Test
+    public void p1()
+    {
+        List<Integer> input = new ArrayList<>();
+        input.add(0);
+        input.add(1);
+        input.add(2);
+
+        List<List<Integer>> result = Permutations.permutations(input, 1);
+        assertTrue( result.size() == factorial(input.size()) / factorial(input.size() - 1));
+
+        List<List<Integer>> expectedResult = new ArrayList<>();
+        List<Integer> e0 = new ArrayList<>();
+        e0.add(0);
+        expectedResult.add(e0);
+
+        List<Integer> e1 = new ArrayList<>();
+        e1.add(1);
+        expectedResult.add(e1);
+
+        List<Integer> e2 = new ArrayList<>();
+        e2.add(2);
+        expectedResult.add(e2);
+
+        assertTrue(result.equals(expectedResult));
+    }
+
+    @Test
+    public void p2()
+    {
+        List<Integer> input = new ArrayList<>();
+        input.add(0);
+        input.add(1);
+        input.add(2);
+
+        int R = 2;
+        List<List<Integer>> result = Permutations.permutations(input, R);
+//        System.err.println(result);
+        assertTrue( result.size() == factorial(input.size()) / factorial(input.size() - R));
+
+        List<List<Integer>> expectedResult = new ArrayList<>();
+        List<Integer> e0 = new ArrayList<>();
+        e0.add(1);
+        e0.add(2);
+        expectedResult.add(e0);
+
+        List<Integer> e1 = new ArrayList<>();
+        e1.add(2);
+        e1.add(1);
+        expectedResult.add(e1);
+
+        List<Integer> e2 = new ArrayList<>();
+        e2.add(0);
+        e2.add(1);
+        expectedResult.add(e2);
+
+        List<Integer> e3 = new ArrayList<>();
+        e3.add(1);
+        e3.add(0);
+        expectedResult.add(e3);
+
+        List<Integer> e4 = new ArrayList<>();
+        e4.add(0);
+        e4.add(2);
+        expectedResult.add(e4);
+
+        List<Integer> e5 = new ArrayList<>();
+        e5.add(2);
+        e5.add(0);
+        expectedResult.add(e5);
+
+        assertTrue(result.equals(expectedResult));
+    }
+
+    @Test
+    public void countTest()
+    {
+        for(int i = 0; i < 100; ++i) {
+            int N = getRandomNumber(2, 10);
+            int R = getRandomNumber(1, N);
+
+            List<Integer> input = new ArrayList<>();
+            for(int n = 0; n < N; ++n) {
+                input.add(n);
+            }
+
+            List<List<Integer>> result = Permutations.permutations(input, R);
+            System.err.println("N: " + N + ", R: " + R + ", count: " + result.size());
+//        System.err.println(result);
+            assertTrue(result.size() == factorial(N) / factorial(N - R));
+        }
+    }
+}
